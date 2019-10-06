5118fed5dfb122e949646b3ec8d27838f0f086b9
kevoroid <kevoroid@gmail.com>
2019-10-06 14:05:57 +0700

    adding new methods to MainContracts interface

--- app/src/main/java/com/kevoroid/forzasample/ui/main/MainContracts.java
+++ app/src/main/java/com/kevoroid/forzasample/ui/main/MainContracts.java
@@ -1,16 +1,25 @@
 package com.kevoroid.forzasample.ui.main;
 
 import android.view.View;
+import com.kevoroid.forzasample.models.Teams;
+
+import java.util.List;
 
 public interface MainContracts {
 
 	interface Views {
+
+		void showLoading();
+
+		void hideLoading();
 
 		void showErr();
 
 		void openTeamDetails();
 
 		View getMainLayout();
+
+		void setupRecyclerView(List<Teams> data);
 
 	}
 
