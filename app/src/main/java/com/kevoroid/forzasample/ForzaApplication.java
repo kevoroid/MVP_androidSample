fbf6eb44b6ad595c2a2a4d9e74ef78667ab2d8e3
kevoroid <kevoroid@gmail.com>
2019-10-06 20:01:29 +0700

    adding context grabber to application class

--- app/src/main/java/com/kevoroid/forzasample/ForzaApplication.java
+++ app/src/main/java/com/kevoroid/forzasample/ForzaApplication.java
@@ -1,11 +1,20 @@
 package com.kevoroid.forzasample;
 
 import android.app.Application;
+import android.content.Context;
 
 public class ForzaApplication extends Application {
+
+	private static Context mContext;
 
 	@Override
 	public void onCreate() {
 		super.onCreate();
+
+		mContext = getApplicationContext();
+	}
+
+	public Context getContext() {
+		return mContext;
 	}
 }
