1b08f0cc89c75b3e7efd6c6e271a48fc0298c7b1
kevoroid <kevoroid@gmail.com>
2019-10-06 11:29:13 +0700

    adding app class and update to manifest

---
+++ app/src/main/java/com/kevoroid/forzasample/ForzaApplication.java
@@ -0,0 +1,11 @@
+package com.kevoroid.forzasample;
+
+import android.app.Application;
+
+public class ForzaApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
